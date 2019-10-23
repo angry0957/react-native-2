df2f2ed678b03a8c37cfe1e9eb80fc3629de40e3
Abdul Rahman <abdul.095722@gmail.com>
2019-10-23 23:16:13 +0500

    Login screen

--- screens/login.tsx
+++ screens/login.tsx
@@ -12,8 +12,10 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Image,
   View,
   Button,
+  TouchableWithoutFeedback,
   TouchableOpacity,
   Text,
   TextInput
@@ -27,10 +29,22 @@ class LoginScreen extends Component {
 
   constructor(props) {
     super(props);
+    this.change = this.change.bind(this);
+    
+    this.state = {
+      email   : '',
+      password: '',
+      isLoading: false,
+      hidePassword: true, 
+    }
   }
 
   login(){
 
+  }
+
+  change(){
+    this.setState({hidePassword: !this.state.hidePassword})
   }
 
   render() {
@@ -39,11 +53,23 @@ class LoginScreen extends Component {
         <View style={{flex: 1/5}}>
           <Text style={{fontSize: 15, flex: 1, textAlignVertical: 'bottom', fontWeight: 'bold'}}>Login</Text>
         </View>
-        <View style={{flex: 2/5, marginTop: 20}}>
+        <View style={{flex: 4/5, marginTop: 20}}>
           <Text>Email</Text>
           <TextInput style={{borderBottomWidth: 1, borderBottomColor: 'gray',}}/>
           <Text style={{marginTop: 30}}>Password</Text>
-          <TextInput style={{borderBottomWidth: 1, borderBottomColor: 'gray', marginBottom: 20}}/>
+          <TextInput 
+            secureTextEntry={this.state.hidePassword}
+            value={this.state.password}
+              onChangeText={ (password) => this.setState({ password }) }
+          style={{borderBottomWidth: 1, borderBottomColor: 'gray', marginBottom: 20}}/>
+          <TouchableWithoutFeedback
+            onPress={this.change}
+            >
+            <Image 
+              source={require('../assets/EyeShape.png')}
+              resizeMode={'contain'}   /* <= changed  */
+              style={{tintColor: '#FF008A', alignSelf: 'flex-end', top: -50, height: 20}} />
+            </TouchableWithoutFeedback>
           <View style={styles.login}>
             <TouchableOpacity
               onPress={this.login}
@@ -51,13 +77,12 @@ class LoginScreen extends Component {
               <Text style={{height: 40, color: 'white', textAlignVertical: 'center', alignSelf: 'center'}}>Login</Text>
             </TouchableOpacity>
           </View>
-        </View>
-        <View style={{flex: 3/5, flexDirection: 'column'}}>
-          <View style={{flex: 1, flexDirection: 'column-reverse', marginBottom: 10}}>
-            <Text style={{ textAlign: 'center', textAlignVertical: 'bottom', justifyContent: 'center'}}>Don't  have an account ?</Text>
-            <Text style={{textAlign: 'center', textAlignVertical: 'bottom', justifyContent: 'center'}}>Become a seller</Text>
+          <View style={{flex: 1}}>
+            <View style={{flex: 1, marginBottom: 10, justifyContent: 'flex-end'}}>
+              <Text style={{color: 'grey', fontSize: 12, textAlign: 'center'}}>Don't  have an account ?</Text>
+              <Text style={{fontSize: 15, textAlign: 'center'}}>Become a seller</Text>
+            </View>
           </View>
-
         </View>
       </View>
   );
